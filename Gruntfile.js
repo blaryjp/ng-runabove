99a1b467dcf7f9ae326a9e6ca5e06c416128c57f
jblary <contact@blary.be>
2014-04-25 22:14:36 +0200

    Add config files

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/**
+ * ngRunabove: Angular Service for RunAbove API
+ *
+ * @author Jean-Philippe Blary (@blary_jp)
+ * @url https://github.com/blaryjp/ng-runabove
+ * @licence MIT
+ */
+
+module.exports = function (grunt) {
+
+    var type = grunt.option('type');
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'clean',
+        'uglify'
+    ]);
+
+    grunt.registerTask('release', [
+        'jshint',
+        'bump-only:' + type,
+        'clean',
+        'uglify',
+        'bump-commit'
+    ]);
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        banner: '/*! ngRunabove v<%= pkg.version %> by Jean-Philippe Blary (@blary_jp) - ' +
+                'https://github.com/blaryjp/ng-runabove - License MIT */\n',
+
+        clean: {
+            files: ['ng-runabove.min.js']
+        },
+
+        jshint: {
+            files: ['ng-runabove.js'],
+            options: {
+                jshintrc: true,
+            },
+        },
+
+        uglify: {
+            js: {
+                src: ['ng-runabove.js'],
+                dest: 'ng-runabove.min.js',
+                options: {
+                    banner: '<%= banner %>'
+                }
+            }
+        },
+
+        bump: {
+            options: {
+                files: ['package.json', 'bower.json'],
+                commit: true,
+                push: false,
+                createTag: true,
+                pushTo: 'upstream',
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                tagName: 'v%VERSION%'
+            }
+        }
+
+    });
+};
